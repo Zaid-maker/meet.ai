dd627bf8fa3a72955d6940e42eb4011cf248c73c
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-06-09 22:40:13 +0500

    feat: enhance authentication flow with improved error handling and user feedback

---
+++ app/auth/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+const Page = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default Page;
---
+++ app/auth/sign-up/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+const Page = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default Page;

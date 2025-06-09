48cd03fb3521e35a598b4a08dd9d7929e97bd646
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-06-09 23:59:10 +0500

    feat: implement sign-in form with validation and user feedback for email and password inputs

--- modules/auth/ui/views/sign-in-view.tsx
+++ modules/auth/ui/views/sign-in-view.tsx
@@ -1,14 +1,76 @@
-"use client"
+"use client";
 
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, { message: "Password is Required" }),
+});
 
 export const SignInView = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Sign In</CardTitle>
-      </CardHeader>
-    </Card>
+    <div className="flex fle-col gap-6">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <Form {...form}>
+            <form action="" className="p-6 md:p-8">
+              <div className="flex fle-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Welcome back</h1>
+                  <p className="text-muted-foreground text-balance">
+                    Login to your account
+                  </p>
+                </div>
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="m@example.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+            </form>
+          </Form>
+
+          <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+            <img src="/logo.svg" alt="Logo" className="h-[92px] w-[92px]" />
+            <p className="text-2xl font-semibold text-white">Meet.AI</p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 };

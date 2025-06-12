4a9ec9961518eb35a8f37b4dbdb1b93f204def7b
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-06-13 03:12:49 +0500

    feat: implement sign-in functionality with error handling and user feedback

--- modules/auth/ui/views/sign-in-view.tsx
+++ modules/auth/ui/views/sign-in-view.tsx
@@ -15,9 +15,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { OctagonAlertIcon } from "lucide-react";
-import { useForm } from "react-hook-form";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -25,6 +28,9 @@ const formSchema = z.object({
 });
 
 export const SignInView = () => {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,12 +39,31 @@ export const SignInView = () => {
     },
   });
 
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    setError(null);
+
+    authClient.signIn.email(
+      {
+        email: data.email,
+        password: data.password,
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: ({ error }) => {
+          setError(error.message);
+        },
+      }
+    );
+  };
+
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
           <Form {...form}>
-            <form action="" className="p-6 md:p-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
               <div className="flex flex-col gap-6">
                 <div className="flex flex-col items-center text-center">
                   <h1 className="text-2xl font-bold">Welcome back</h1>

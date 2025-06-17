a12f73809667935118aa8d3b447529021f225db8
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-06-18 02:06:30 +0500

    feat: simplify social login handling in sign-in view

--- modules/auth/ui/views/sign-in-view.tsx
+++ modules/auth/ui/views/sign-in-view.tsx
@@ -61,6 +61,26 @@ export const SignInView = () => {
     );
   };
 
+  const onSocial = (provider: "github" | "google") => {
+    setError(null);
+    setPending(true);
+
+    authClient.signIn.social(
+      {
+        provider: provider,
+      },
+      {
+        onSuccess: () => {
+          setPending(false);
+          router.push("/");
+        },
+        onError: ({ error }) => {
+          setError(error.message);
+        },
+      }
+    );
+  };
+
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
@@ -127,11 +147,7 @@ export const SignInView = () => {
                 <div className="grid grid-cols-2 gap-4">
                   <Button
                     disabled={pending}
-                    onClick={() => {
-                      authClient.signIn.social({
-                        provider: "google",
-                      });
-                    }}
+                    onClick={() => onSocial("google")}
                     variant={"outline"}
                     type="button"
                     className="w-full"
@@ -140,11 +156,7 @@ export const SignInView = () => {
                   </Button>
                   <Button
                     disabled={pending}
-                    onClick={() => {
-                      authClient.signIn.social({
-                        provider: "github",
-                      });
-                    }}
+                    onClick={() => onSocial("github")}
                     variant={"outline"}
                     type="button"
                     className="w-full"

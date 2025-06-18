b60b664bd2b1f039425c74a654679d893b412c8d
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-06-18 20:19:14 +0500

    feat: refactor page component to remove sign-up functionality and streamline session handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,21 @@
 "use client";
 
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 
-export default function Home() {
+const Page = () => {
   const { data: session } = authClient.useSession();
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSubmit = () => {
-    authClient.signUp.email(
-      {
-        name,
-        email,
-        password,
-      },
-      {
-        onError: () => {
-          window.alert("Error");
-        },
-        onSuccess: () => {
-          window.alert("Success");
-        },
-      }
-    );
-  };
-
-  if (session) {
-    return (
-      <div className="p-4 flex flex-col gap-y-4">
-        <div>Logged in as {session.user.name}</div>
-        <Button onClick={() => authClient.signOut()}>Sign out</Button>
-      </div>
-    );
+  if (!session) {
+    return <p>Loading...</p>;
   }
 
   return (
-    <div className="p-4 flex flex-col gap-y-4">
-      <Input
-        placeholder="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Input
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        placeholder="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={onSubmit}>Submit</Button>
+    <div className="flex flex-col p-4 gap-y-4">
+      <p>Logged in as {session.user.name}</p>
+      <Button onClick={() => authClient.signOut()}>Sign out</Button>
     </div>
   );
-}
+};
+
+export default Page;

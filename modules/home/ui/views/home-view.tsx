9eb785c44d0c87f89aeaa70f9429926f3f4e3c12
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-06-23 02:14:10 +0500

    feat: remove unused TRPC hello procedure and simplify HomeView component

--- modules/home/ui/views/home-view.tsx
+++ modules/home/ui/views/home-view.tsx
@@ -1,23 +1,5 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data, isLoading, error } = useQuery(
-    trpc.hello.queryOptions({ text: "world" })
-  );
-
-  return (
-    <div className="flex flex-col p-4 gap-y-4">
-      {error ? (
-        <div className="text-red-500">Error: {error.message}</div>
-      ) : isLoading ? (
-        <div className="animate-pulse">Loading...</div>
-      ) : (
-        data?.greeting
-      )}
-    </div>
-  );
+  return <div className="flex flex-col p-4 gap-y-4">Home View</div>;
 };
